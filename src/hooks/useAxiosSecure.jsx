e03b8b34d5212c1b200d724f141467c8337819fc
chaitey50 <chaiteyc@gmail.com>
2025-09-07 20:14:28 +0600

    update useAxiosSecure file

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,19 +1,37 @@
 import axios from 'axios';
-import React, { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from '../Contexts/AuthContext/AuthContext';
- const axiosInstance = axios.create({
-        baseURL:import.meta.env.VITE_BASE_URL
-    }) 
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+});
 
 const useAxiosSecure = () => {
-   
-const {user} = useContext(AuthContext);
-axiosInstance.interceptors.request.use(config => {
-   if(user?.accessToken){
-     config.headers.authorization = `Bearer ${user.accessToken}`
-   }
-    return config;
-})
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+      
+        const requestInterceptor = axiosInstance.interceptors.request.use(config => {
+            if (user?.accessToken) {
+                config.headers.authorization = `Bearer ${user.accessToken}`;
+            }
+            return config;
+        }, error => Promise.reject(error));
+
+        const responseInterceptor = axiosInstance.interceptors.response.use(
+            response => response,
+            error => {
+                if (error.response && error.response.status === 401) {
+                    console.log("Unauthorized! Token may have expired.");
+                }
+                return Promise.reject(error);
+            }
+        );
+        return () => {
+            axiosInstance.interceptors.request.eject(requestInterceptor);
+            axiosInstance.interceptors.response.eject(responseInterceptor);
+        };
+    }, [user]);
 
     return axiosInstance;
 };
